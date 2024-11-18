88c8e9222de883202b137e0f82ea5219e8e7b17c
showard1793 <Showard1793@gmail.com>
2024-11-17 21:26:38 -0600

    Updated:RegistrationFormDTO

---
+++ src/main/java/org/launchcode/techjobsauth/models/dto/RegistrationFormDTO.java
@@ -0,0 +1,7 @@
+//YOUTUBE LINK:https://youtu.be/i8Swv5d1IBY?si=uuLHSiWEnr8XuNVR&t=820
+
+package org.launchcode.techjobsauth.models.dto;
+
+public class RegistrationFormDTO extends LoginFormDTO{
+
+}
